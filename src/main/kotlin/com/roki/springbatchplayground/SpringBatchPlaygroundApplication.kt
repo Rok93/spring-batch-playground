05d01335f1d3875eb501808e8b4bbee44eba6800
GyeongrokKim <kr.kim@mfort.co.kr>
2022-10-21 17:49:49 +0900

    Initial commit

---
+++ src/main/kotlin/com/roki/springbatchplayground/SpringBatchPlaygroundApplication.kt
@@ -0,0 +1,11 @@
+package com.roki.springbatchplayground
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBatchPlaygroundApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBatchPlaygroundApplication>(*args)
+}
---
+++ src/test/kotlin/com/roki/springbatchplayground/SpringBatchPlaygroundApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.roki.springbatchplayground
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBatchPlaygroundApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
